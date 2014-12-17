b6c8f46f2b8f0eed065823d013cd06f6f16fff30
Christof <chrkon@mail.de>
2014-12-18 00:10:13 +0100

    Abhängigkeiten reduzier .net40

--- Source/HelixToolkit.HID.SpaceNavigator.Wpf/SpaceNavigatorDecorator.cs
+++ Source/HelixToolkit.HID.SpaceNavigator.Wpf/SpaceNavigatorDecorator.cs
@@ -14,10 +14,55 @@ namespace HelixToolkit.HID.SpaceNavigator.Wpf
     using System.Windows;
     using System.Windows.Controls;
 
-    using HelixToolkit.HID.SpaceNavigator;
     using HelixToolkit.Wpf;
     using TDx.TDxInput;
 
+    /// <summary>
+    /// Space navigator type enumeration.
+    /// </summary>
+    public enum SpaceNavigatorType
+    {
+        /// <summary>
+        /// Is a unknown device.
+        /// </summary>
+        UnknownDevice = 0,
+
+        /// <summary>
+        /// Is a SpaceNavigator.
+        /// </summary>
+        SpaceNavigator = 6,
+
+        /// <summary>
+        /// Is a SpaceExplorer.
+        /// </summary>
+        SpaceExplorer = 4,
+
+        /// <summary>
+        /// Is a SpaceTraveler.
+        /// </summary>
+        SpaceTraveler = 25,
+
+        /// <summary>
+        /// Is a SpacePilot.
+        /// </summary>
+        SpacePilot = 29
+    }
+
+    /// <summary>
+    /// Zoom mode.
+    /// </summary>
+    public enum SpaceNavigatorZoomMode
+    {
+        /// <summary>
+        /// In and out.
+        /// </summary>
+        InOut,
+
+        /// <summary>
+        /// Up and down.
+        /// </summary>
+        UpDown
+    }
 
     /// <summary>
     /// A decorator for the space navigator.
