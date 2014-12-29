b1e53d0bddc8817aa87a33de7dd829eae3341ed7
Christof <chrkon@mail.de>
2014-12-29 06:57:39 +0100

    CameraControl Interface added to HelixToolkit

---
+++ Source/HelixToolkit/ICameraControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelixToolkit
+{
+    public interface ICameraControl
+    {
+        void Move(double dx = 0.0, double dy = 0.0, double dz = 0.0);
+        void Pan(double dx = 0.0, double dy = 0.0, double dz = 0.0);
+        void Rotate(double dx = 0.0, double dy = 0.0);
+        void Zoom(double delta = 0.0);
+    }
+}
