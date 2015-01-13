99d24f0aa18a892762c1b5dbf49e1e39696f4e7e
ck <konstantinopoulos@glm-laser.com>
2015-01-13 08:44:08 +0100

    first commit of the new HID Interface

---
+++ Source/HelixToolkit/IHumanInterfaceDevice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelixToolkit
+{
+    interface IHumanInterfaceDevice
+    {
+        /// <summary>
+        /// Camera Track = moving camera horizontal left/right (perpendicular to ViewDirection)
+        /// </summary>
+        event Action<double> CameraTrack; 
+        
+        /// <summary>
+        /// Camera Crane = moving camera vertical up/down (perpendicular to ViewDirection)
+        /// </summary>
+        event Action<double> CameraCrane; 
+        
+        /// <summary>
+        /// Camera Dolly = moving camera horizontal forward/backward (along ViewDirection)
+        /// </summary>
+        event Action<double> CameraDolly;
+
+        /// <summary>
+        /// Camera Panning = turning camera left/right around Updirection (Aeroplane : Yaw)
+        /// only ViewDirection will be changed
+        /// </summary>
+        event Action<double> CameraPan;
+
+        /// <summary>
+        /// Camera Tilt = turning camera up/down around Vector perpendicular to UpDirection and ViewDirection (Aeroplane : Pitch)
+        /// ViewDirection and UpDirection will be changed
+        /// </summary>
+        event Action<double> CameraTilt;
+
+        /// <summary>
+        /// Camera Rolling = rotate camera left/right around ViewDirection (Aeroplane : Roll)
+        /// only UpDirection will be changed
+        /// </summary>
+        event Action<double> CameraRoll;
+
+        /// <summary>
+        /// Camera Zoom = increase/decrease Field of View angle
+        /// </summary>
+        event Action<double> CameraZoom;
+
+
+    }
+}
